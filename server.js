f43474bb8bd88d266555d47fa5a4ca5deeca221c
Park, Sojeong <sojeong2101@gmail.com>
2025-04-01 11:13:48 +0900

    [Feat] MongDB 연결

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
+require("dotenv").config();
+
+const connectDB = require("./database.js");
+
+let db;
+connectDB
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db(process.env.DB_NAME);
+    app.listen(process.env.PORT, () => {
+      console.log("서버 실행 중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (요청, 응답) => {
   응답.send("Hello, World!");
