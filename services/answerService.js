dbed6e774e8b9ebeafeaf50da8dd65f4a77303f1
Park, Sojeong <sojeong2101@gmail.com>
2025-04-25 09:17:26 +0900

    [Feat] 월별 응답 조회 기능 추가 및 라우터 경로 변경
    - 응답 데이터에서 응답 ID 및 일(day)만 추출하여 캘린더 dot 표시용으로 활용
    - year, month 쿼리스트링으로 조회
    
    관련: [Feature] 응답(답변) 월별 응답 목록 조회 API 구현 #27

--- services/answerService.js
+++ services/answerService.js
@@ -16,7 +16,12 @@ const getAllAnswersByUser = async (userId) => {
   return await answerRepository.findAllAnswersByUserId(userId);
 };
 
+const getAnswersByYearAndMonth = async (userId, year, month) => {
+  return await answerRepository.findAnswersByUserAndYearAndMonth(userId, year, month);
+};
+
 module.exports = {
   createAnswer,
   getAllAnswersByUser,
+  getAnswersByYearAndMonth,
 };
